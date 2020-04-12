a45dca34e11f2814ac48740eef317f5e3083e0b5
Hugo Avila <hugoaf@yahoo.com.mx>
2020-04-12 15:20:28 -0700

    my solution
    https://holy-sulky-gilmoreosaurus.glitch.me

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
-
+var express = require("express");
+var cors = require("cors");
 // require and use "multer"...
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+//app.use("/api/fileanalyse", fileupload());
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (
+  req,
+  res,
+  next
+) {
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
   });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
